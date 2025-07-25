0a07ceda57ab9722d818f49ada93446ac6df90d1
saikumar078 <saikumarenagandula1999@gmail.com>
2025-07-25 14:39:03 +0530

    v0.0.1

---
+++ src/modules/locked-asset-factory/locked-asset.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AbiLockedAssetService } from './services/abi-locked-asset.service';
+import { ContextModule } from '../../services/context/context.module';
+import { DRTCommunicationModule } from '../../services/dharitri-communication/drt.communication.module';
+import { LockedAssetGetterService } from './services/locked.asset.getter.service';
+import { TokenModule } from '../tokens/token.module';
+
+@Module({
+    imports: [DRTCommunicationModule, ContextModule, TokenModule],
+    providers: [AbiLockedAssetService, LockedAssetGetterService],
+    exports: [LockedAssetGetterService],
+})
+export class LockedAssetModule {}
---
+++ src/modules/tokens/token.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { TokenService } from './services/token.service';
+import { DatabaseModule } from 'src/services/database/database.module';
+import { DRTCommunicationModule } from 'src/services/dharitri-communication/drt.communication.module';
+
+@Module({
+    imports: [DRTCommunicationModule, DatabaseModule],
+    providers: [TokenService],
+    exports: [TokenService],
+})
+export class TokenModule {}
